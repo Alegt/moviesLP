8f8e370fc2cef402fc5758966c8c85a412bd7e38
Alejandro Gomez <agomez@backcountry.com>
2015-07-02 17:49:59 -0600

    Live person integration module test

---
+++ liveperson/iphone/Classes/ComLivepersonModule.h
@@ -0,0 +1,14 @@
+/**
+ * liveperson
+ *
+ * Created by Alejandro Gomez
+ * Copyright (c) 2015 . All rights reserved.
+ */
+
+#import "TiModule.h"
+
+@interface ComLivepersonModule : TiModule
+{
+}
+
+@end
